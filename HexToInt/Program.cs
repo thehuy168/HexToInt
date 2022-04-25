d4a0228586969548427c04f692516f8ad0b3f205
TheHuy <TheHuy@HUYPC>
2022-04-25 11:01:30 +0700

    Add project files.

---
+++ HexToInt/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("input Hexadeximon");
+string? hexval = Console.ReadLine();
+if (hexval == null)
+{
+    hexval = "0";
+}
+try
+{
+    Console.WriteLine("Hexadecimal number: " + hexval);
+    int decValue = int.Parse(hexval, System.Globalization.NumberStyles.HexNumber);
+    Console.WriteLine("Convert to-");
+    Console.WriteLine("Decimal number: " + decValue);
+}
+catch
+{
+    Console.WriteLine($"{hexval}" + "  is not hexadeximon format");
+
+}
